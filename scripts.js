2872761cf167509b980380f6b6da39087b2891bc
Split Flap <splitflappin@gmail.com>
2026-01-17 00:06:05 -0500

    added div and p's to show the results and score
    - created a div to store 2 paragraphs
    - paragraph is displaying the choices
    - paragraph showing the current score

--- scripts.js
+++ scripts.js
@@ -13,6 +13,8 @@ function getComputerChoice() {
 const rockBtn = document.querySelector(".rock");
 const paperBtn = document.querySelector(".paper");
 const scissorsBtn = document.querySelector(".scissors");
+const resultEl = document.querySelector(".result");
+const scoreEl = document.querySelector(".score");
 
 rockBtn.addEventListener("click", (e) => {
   playRound("Rock", getComputerChoice());
@@ -26,24 +28,28 @@ scissorsBtn.addEventListener("click", (e) => {
   playRound("Scissors", getComputerChoice());
 });
 
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    console.log(`Tie, you both selected ${humanChoice}`);
-  } else if ((humanChoice === "Paper") & (computerChoice == "Rock")) {
-    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-  } else if ((humanChoice === "Scissors") & (computerChoice === "Paper")) {
-    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-  } else if (humanChoice === "Rock" && computerChoice == "Scissors") {
-    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-  } else {
-    console.log(`You lose! ${humanChoice} beats ${computerChoice}`);
-  }
-
-  computerSelection = getComputerChoice();
-}
-
-let computerSelection = getComputerChoice();
-
 let humanScore = 0;
 let computerScore = 0;
-let rounds = 0;
+
+function playRound(humanChoice, computerChoice) {
+  resultEl.textContent = "";
+  if (humanChoice === computerChoice) {
+    humanScore += 1;
+    computerScore += 1;
+    resultEl.textContent = `Tie, you both selected ${humanChoice}`;
+  } else if ((humanChoice === "Paper") & (computerChoice === "Rock")) {
+    resultEl.textContent = `You win! Your pick ${humanChoice} beats ${computerChoice}`;
+    humanScore += 1;
+  } else if ((humanChoice === "Scissors") & (computerChoice === "Paper")) {
+    resultEl.textContent = `You win! Your pick ${humanChoice} beats ${computerChoice}`;
+    humanScore += 1;
+  } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+    humanScore += 1;
+    resultEl.textContent = `You win! Your pick ${humanChoice} beats ${computerChoice}`;
+  } else {
+    resultEl.textContent = `You lose! Your pick ${humanChoice} loses to ${computerChoice}`;
+    computerScore += 1;
+  }
+
+  scoreEl.textContent = `Your score ${humanScore} Computer score ${computerScore}`;
+}
