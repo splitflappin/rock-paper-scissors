53ca642bb4f2a687e29a137afdf9918e02f4a0c7
Split Flap <splitflappin@gmail.com>
2026-01-16 23:02:20 -0500

    removed the game tracking logic and how the human makes the choice
    i removed the logic that keeps track of the rounds and how the human makes a choice.
    i replaced it buttons and listeners to track how a makes a selection

--- scripts.js
+++ scripts.js
@@ -10,62 +10,40 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice(instructions = "Rock Paper or Scissors?") {
-  let pick = prompt(instructions);
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorsBtn = document.querySelector(".scissors");
 
-  if (pick.toLowerCase() === "rock") {
-    return "Rock";
-  } else if (pick.toLowerCase() === "paper") {
-    return "Paper";
-  } else if (pick.toLowerCase() === "scissors") {
-    return "Scissors";
-  } else {
-    getHumanChoice("Invalid choice. Must be either paper, rock, or scissors");
-  }
-}
+rockBtn.addEventListener("click", (e) => {
+  playRound("Rock", getComputerChoice());
+});
+
+paperBtn.addEventListener("click", (e) => {
+  playRound("Paper", getComputerChoice());
+});
+
+scissorsBtn.addEventListener("click", (e) => {
+  playRound("Scissors", getComputerChoice());
+});
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
     console.log(`Tie, you both selected ${humanChoice}`);
   } else if ((humanChoice === "Paper") & (computerChoice == "Rock")) {
-    humanScore += 1;
     console.log(`You win! ${humanChoice} beats ${computerChoice}`);
   } else if ((humanChoice === "Scissors") & (computerChoice === "Paper")) {
-    humanScore += 1;
     console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-  } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
-    humanScore += 1;
+  } else if (humanChoice === "Rock" && computerChoice == "Scissors") {
     console.log(`You win! ${humanChoice} beats ${computerChoice}`);
   } else {
-    computerScore += 1;
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    console.log(`You lose! ${humanChoice} beats ${computerChoice}`);
   }
 
-  humanSelection = getHumanChoice();
   computerSelection = getComputerChoice();
-
-  playGame();
-}
-function playGame() {
-  if (rounds <= 5) {
-    rounds += 1;
-    playRound(humanSelection, computerSelection);
-  } else {
-    if (humanScore > computerScore) {
-      return console.log("Game over: you win!");
-    } else if (humanScore === computerScore) {
-      return console.log("Game over: it's a tie");
-    } else {
-      return console.log("Game over: you lose!");
-    }
-  }
 }
 
-let humanSelection = getHumanChoice();
 let computerSelection = getComputerChoice();
 
 let humanScore = 0;
 let computerScore = 0;
 let rounds = 0;
-
-playGame();
